f7bcea248af385854ef4b6c3941116f93100dddf
Emmanuel <emmanuelpalacio1@hotmail.com>
2020-08-13 22:00:09 +0000

    command process without path, not yet completed

--- Analizar_String.c
+++ Analizar_String.c
@@ -34,11 +34,53 @@ int _Fork(char **argv)
 		wait(&status);
 	return (0);
 }
+
+char *Match_Path(char *Command_Or_Path, List *list)
+{
+	Element *element = list->Head;
+
+	while (element)
+	{
+		if (!(list->Match(element->Path, Command_Or_Path)))
+			return (Command_Or_Path);
+		element = element->Next;
+	}
+	return (NULL);
+}
+
+char *_strcat(Element *element, char *Command)
+{
+	int Length1 = 0;
+	char *Path;
+	int i = 0, j = 0;
+
+	while (Command[Length1])
+		Length1++;
+	Length1 += 2;
+	Length1 += element->Length_Path;
+
+	Path = (char *)malloc(sizeof(char) * Length1);
+
+	while (i < Length1)
+	{
+		if (i < element->Length_Path)
+			Path[i] = (element->Path)[i];
+		else if (i == element->Length_Path)
+			Path[i] = '/';
+		else
+			Path[i] = Command[j++];
+		i++;
+	}
+	Path[i] = '\0';
+	return (Path);
+}
+
 void Analizar_String(char *String_Character, List *list)
 {
 	int Number_Word = Counter_Word(String_Character);
 	int Index       = 1;
 	char **argv     = (char **)malloc(sizeof(char *) * (Number_Word + 1));
+	char *Temp;
 
 	if (!argv)
 		return ;
@@ -46,6 +88,22 @@ void Analizar_String(char *String_Character, List *list)
 	while (Index < Number_Word)
 		argv[Index++] = strtok(NULL, " \n\t");
 
-	_Fork(argv);
+	Temp = *argv;
+	if (Temp = Match_Path(*argv, list))
+		_Fork(argv);
+	else
+	{
+		Element *element = list->Head;
+		char *Temp_String;
+		struct stat st;
+
+		while (element)
+		{
+			Temp_String = _strcat(element, *argv);
+			list->Execve(Temp, argv);
+			free(Temp_String);
+		}
+		free(Temp_String);
+	}
 	free (argv);
 }
